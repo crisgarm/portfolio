23e831368791eb55481547d8221308b713226ee9
crisgarm <crisgarm9@gmail.com>
2021-01-18 17:45:53 +0100

    Add components of main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,17 @@
 import React from "react";
+import AboutMe from "./AboutMe";
+import ProjectList from "./ProjectList";
+import Contact from "./Contact";
+import "../stylesheets/Main.scss";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <main>
+      <AboutMe />
+      <ProjectList />
+      <Contact />
+    </main>
+  );
 };
 
 export default Main;
