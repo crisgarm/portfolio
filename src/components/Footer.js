0a523c7f801e3eb5b53b950c9521afab0dd65034
crisgarm <crisgarm9@gmail.com>
2021-01-18 20:54:34 +0100

    Add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "../stylesheets/Footer.scss";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className="footer">
+      <small className="footer__text">
+        &lt; Desarrollado por Cristina García Martín /&gt;
+      </small>
+    </footer>
+  );
 };
 
 export default Footer;
